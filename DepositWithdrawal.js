78656afa431f85c4267f7aceaacfcd822ad1a041
unknown <spinnavor@naver.com>
2023-02-07 16:52:14 +0900

    [ADD] Add csv as database and interconnect with frontend

--- DepositWithdrawal.js
+++ DepositWithdrawal.js
@@ -1,11 +1,12 @@
+const fs = require('fs');
 
-let idxnum=0;
+let idxnum=1;
 
 function setDate() {
   document.getElementById('now_date').valueAsDate = new Date();
 }
 
-function setDWIndex(num = 0) {
+function setDWIndex(num = idxnum) {
   document.getElementById('dwindex').value = num;
 }
 
@@ -31,9 +32,7 @@ function onEditTransaction() {
   let existingIdx = false;
   let curRow = 0;
   let rowIdx = table.rows.length - 1;
-  console.log(`rowIdx : ${rowIdx}`)
   if(rowIdx > 0){
-    console.log(transaction.dwindex)
     for (let i = 0; i < rowIdx; i++){
       if (hTbody.rows[i].cells[0].innerHTML == transaction.dwindex){
         existingIdx = true;
@@ -46,12 +45,14 @@ function onEditTransaction() {
   }
 
   if(existingIdx){
+    modifyCSV(curRow + 1,transaction);
     setRow(hTbody,curRow,transaction);
   }
   else if(!existingIdx){
     
     addRow(hTbody, 0);
-    setRow(hTbody,0,transaction)
+    setRow(hTbody,0,transaction);
+    addToCSV(transaction);
     idxnum++;
   }
   
@@ -69,6 +70,17 @@ function setRow(table, row, rowdata){
   table.rows[row].cells[7].innerHTML = rowdata.note;
 }
 
+function fetchRow(table, row, rowdata){
+  table.rows[row].cells[0].innerHTML = rowdata[0];
+  table.rows[row].cells[1].innerHTML = rowdata[1];
+  table.rows[row].cells[2].innerHTML = rowdata[2] || 0;
+  table.rows[row].cells[3].innerHTML = rowdata[3] || '-';
+  table.rows[row].cells[4].innerHTML = rowdata[4] || 0;
+  table.rows[row].cells[5].innerHTML = rowdata[5] || 0;
+  table.rows[row].cells[6].innerHTML = rowdata[6];
+  table.rows[row].cells[7].innerHTML = rowdata[7];
+}
+
 function addRow(table, idx = 0) {
   const newRow0 = table.insertRow(idx); 
   const newCell0 = newRow0.insertCell();
@@ -81,5 +93,54 @@ function addRow(table, idx = 0) {
   const newCell7 = newRow0.insertCell();
 }
 
+function addToCSV(rowdata) {
+  fs.appendFile(__dirname+ '../../../data/ledger.csv', `${rowdata.dwindex},${rowdata.date},${rowdata.code || 0},${rowdata.item || '-'},${rowdata.deposit || 0},${rowdata.withdraw || 0},${rowdata.stock},${rowdata.note}\n`,'utf-8', (err) => {console.log(err)});
+  console.log('Transaction Added');
+}
+
+function modifyCSV(curRow, rowdata) {
+  const hTbody = document.getElementById('htmlTbody');
+  const table = document.getElementById('dwTable');
+  const row = hTbody.rows.length;
+
+  console.log(`row length : ${row}`)
+  for(let i = row; i > 0; i--){
+    if(i != curRow){
+      fs.appendFile(__dirname+ '../../../data/ledgerTemp.csv', `${table.rows[i].cells[0].innerHTML},${table.rows[i].cells[1].innerHTML},${table.rows[i].cells[2].innerHTML},${table.rows[i].cells[3].innerHTML},${table.rows[i].cells[4].innerHTML},${table.rows[i].cells[5].innerHTML},${table.rows[i].cells[6].innerHTML},${table.rows[i].cells[7].innerHTML}`, 'utf-8', (err) => {})
+    }
+    else if(i == curRow){
+      fs.appendFile(__dirname+ '../../../data/ledgerTemp.csv', `${rowdata.dwindex},${rowdata.date},${rowdata.code || 0},${rowdata.item || '-'},${rowdata.deposit || 0},${rowdata.withdraw || 0},${rowdata.stock},${rowdata.note}\n`,'utf-8', (err) => {console.log(err)});
+    }
+  }
+  fs.unlink('./data/ledger.csv', (err) => {"fail to delete \"ledger.csv\""});
+  fs.rename(__dirname+ '../../../data/ledgerTemp.csv', __dirname+ '../../../data/ledger.csv', (err) => {"fail to rename \"ledgerTemp.csv\""});
+  fs.open(__dirname+ '../../../data/ledgerTemp.csv','w', (err) => {"fail to create \"ledgerTemp.csv\""});
+  console.log("modifyied")
+}
+
+function readFromCSV() {
+  const hTbody = document.getElementById('htmlTbody');
+  const transaction = getTransactionInput();
+
+  fs.open('./data/ledger.csv', 'r', (err, fd)=>{
+    fs.readFile(fd, 'utf-8', (err,data) =>{
+      const row = data.split("\n");
+      const table = [];
+      for (let i = 0; i < row.length - 1; i++) {
+        let eachrow = {};
+        eachrow = row[i].split(',');
+        addRow(hTbody, 0);
+        fetchRow(hTbody, 0, eachrow)
+         if (i == row.length - 1){
+           idxnum = eachrow[0];
+           idxnum++;
+           setDWIndex(idxnum);
+         }
+      }
+    })
+  })
+}
+
 setDate();
+readFromCSV();
 setDWIndex();
--- main.js
+++ main.js
@@ -2,7 +2,6 @@
 
 const electron = require('electron');
 const fs = require('fs');
-const path = require('path');
 
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -25,40 +24,7 @@ app.on('ready', () => {
         //enableRemoteModule: true,
       }
     });
-    const ledger_csv = fs.readFileSync(__dirname + "\\data\\result.csv","utf-8");
-    csvRows(ledger_csv);
     mainWindow.loadURL(`file://${app.getAppPath()}/static/html/ledger.html`);
     console.log('Welcome to HIRP!')
     mainWindow.on('closed', () => { mainWindow = null; });
 });
-
-function csvRows (csv) {
-  const arr = [['품목', '나이'], ['제로초','27'], ['네로', '32']];
-  const data = arr.map((row) => row.join(',')).join('\n');
-  fs.writeFileSync('./data/result.csv', data,"utf-8");
-
-  const rows = csv.split("\r\n");
-  if(rows[rows.length - 1] === ''){
-    rows.pop();
-  }
-  console.log(rows)
-
-  let results = [];
-  let columnTitle = [];
-
-  for (const i in rows) {
-    const row = rows[i]
-    const data = row.split(",")
-    if (i === "0"){
-      columnTitle = data;
-    } else {
-      let row_data = {};
-      for (const index in columnTitle) {
-        const title = columnTitle[index];
-        row_data[title] = data[index];
-      }
-      results.push(row_data);
-    }
-  }
-  console.log(results);
-}
